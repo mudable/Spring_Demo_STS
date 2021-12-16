6876a87ee56bdb15e5829dcdd752c2f6764345d2
mudable <mudableshilpa@gmail.com>
2021-12-16 11:46:02 +0530

    first commit

---
+++ src/main/java/com/example/demo/HelloworldDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldDemoApplication {
+	/*
+	 * @SpringBootApplication:describs the start of spring project.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello from BridgeLabz");
+		SpringApplication.run(HelloworldDemoApplication.class, args);
+	}
+
+}
